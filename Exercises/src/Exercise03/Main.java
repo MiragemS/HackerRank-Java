9ffd65c7ecb26a2a4ebcedaed2f643443ad93a4d
MiragemS <musantos016@gmail.com>
2025-04-09 18:42:05 -0300

    novos exercícios 3 a 5

--- Exercises/src/Exercise03/Main.java
+++ Exercises/src/Exercise03/Main.java
@@ -7,12 +7,15 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+        int i = 1;
         try {
-            File documento = new File("Exercise03/doc.txt");
+            File documento = new File("Exercises/src/Exercise03/text.txt");
+            System.out.println("Caminho absoluto: " + documento.getAbsolutePath());
             Scanner leitor = new Scanner(documento);
             while (leitor.hasNextLine()) {
                 String data = leitor.nextLine();
-                System.out.println(data);
+                System.out.println(i + " " + data);
+                i++;
             }
             leitor.close();
         } catch (FileNotFoundException e) {
